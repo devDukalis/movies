a24a2e2628d24155ddddb24166dd941b098235d2
devdukalis <devdukalis@gmail.com>
2025-02-10 13:50:35 +0300

    fix: rating badge border color

--- src/components/RatingBadge/RatingBadge.jsx
+++ src/components/RatingBadge/RatingBadge.jsx
@@ -1,7 +1,11 @@
+import { getStarColor } from "../../utils/index"
+
 import "./styles.css"
 
 const RatingBadge = ({ rating }) => (
-  <div className="rating-badge">{rating?.toFixed(1) || "0.0"}</div>
+  <div className="rating-badge" style={{ border: `2px solid ${getStarColor(rating)}` }}>
+    {rating?.toFixed(1) || "0.0"}
+  </div>
 )
 
 export default RatingBadge
